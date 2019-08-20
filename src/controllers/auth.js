c8466765ad10d0e58570408fb221e1aabfb0e52b
Stanley Huang <huang.stanley050@gmail.com>
2019-08-20 19:32:59 +1000

    login route also done

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,18 +5,35 @@ const register_end_point = `https://identitytoolkit.googleapis.com/v1/accounts:s
 
 export default {
   login: async (req, res, next) => {
-    res.json({ message: "Login route" });
-  },
-  register: async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(email, password);
+    let login_result;
     const data = {
       email,
       password,
       returnSecureToken: true
     };
+    try {
+      login_result = await axios.post(login_end_point, data);
+    } catch (err) {
+      console.log(err.response);
+      const error = new Error("Unable to login");
+      error.statusCode = 401;
+      return next(error);
+    }
+    res.json({ message: "Login successful", data: login_result.data });
+  },
+  register: async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
     let register_result;
+
+    const data = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
     try {
       register_result = await axios.post(register_end_point, data);
     } catch (err) {
