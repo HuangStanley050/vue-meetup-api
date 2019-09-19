3421c628f0b3655818343be47fb3a26bdb6ee720
Stanley Huang <huang.stanley050@gmail.com>
2019-09-19 20:51:52 +1000

    tested the login route, mocking axios call to google

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -1,2 +1,37 @@
 import app from "../app";
 import request from "supertest";
+import axios from "axios";
+
+jest.mock("axios");
+axios.post.mockImplementation((route, data) => {
+  //console.log(data);
+  if (data.email === "test@test.com" && data.password === "1234") {
+    return Promise.resolve({ data: { name: "stuff" } });
+  } else {
+    return Promise.reject({
+      response: {
+        data: {
+          error: {
+            message: "You have failed to login"
+          }
+        }
+      }
+    });
+  }
+});
+
+test("To see if the login route fails with incorrect password", async done => {
+  let login_result = await request(app)
+    .post("/api/auth/login")
+    .send({ email: "test@test.com", password: "123" });
+  expect(login_result.statusCode).toBe(401);
+  done();
+});
+
+test("To see if the login route succeed with correct password", async done => {
+  let login_result = await request(app)
+    .post("/api/auth/login")
+    .send({ email: "test@test.com", password: "1234" });
+  expect(login_result.statusCode).toBe(200);
+  done();
+});
